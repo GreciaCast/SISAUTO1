dfd99dfb9b3fa33e048907ba3d8739334aed9851
GreCastillo <33234876+GreCastillo@users.noreply.github.com>
2018-12-21 00:04:41 -0600

    Validacion de no dar debaja a producto si tiene existencias en inventario

--- view/funciones.php
+++ view/funciones.php
@@ -13,4 +13,27 @@ function contarProducto($id){
 
 }
 
+function contarProductoInventario($id){
+	$conexion = conectarMysql();
+
+	$sql = "SELECT * from inventario where id_Producto = '$id' ";
+	$var = mysqli_query($conexion,$sql);
+
+	$total = mysqli_num_rows($var);
+
+	if(!($total == 0)){
+		$sql2 = "SELECT nuevaExistencia_Inv FROM inventario WHERE id_Producto = '$id' order by idInventario desc";
+		$resultadooS = mysqli_query($conexion,$sql2) or die ("Error a Conectar en la BD".mysqli_connect_error());
+		$resultadoo = mysqli_fetch_array($resultadooS);//CAPTURA EL ULTIMO REGISTRO
+		$cant = $resultadoo['nuevaExistencia_Inv'];
+		if($cant == 0){
+			$total = 0;
+		}else{
+			$total = 1;
+		}
+	}
+	return $total; 
+
+}
+
 ?>
